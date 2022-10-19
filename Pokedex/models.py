14f1f5cf2a3046bfd915e88f3bf70551d9e5b7c1
OHerrera1991 <Herreraomar791@gmail.com>
2022-10-19 19:42:06 -0300

    Templates2

--- Pokedex/models.py
+++ Pokedex/models.py
@@ -1,5 +1,6 @@
 from turtle import width
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,4 +12,8 @@ class Pokemon(models.Model):
     ability=models.CharField(max_length=30)
     weak=models.CharField(max_length=30)
     description=models.CharField(max_length=300)
-    
+
+class Avatar(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    imagen = models.ImageField(upload_to='avatar/', null=True, blank=True)
+
