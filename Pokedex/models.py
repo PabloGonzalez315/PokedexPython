e9f26a801716c1905d11967375036fb3c9984694
OHerrera1991 <Herreraomar791@gmail.com>
2022-10-20 01:04:51 -0300

    Login/Logout

--- Pokedex/models.py
+++ Pokedex/models.py
@@ -1,18 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Pokemon(models.Model):
-    number=models.IntegerField()
-    name=models.CharField(max_length=30)
-    type1=models.CharField(max_length=30)
-    type2=models.CharField(max_length=30)
-    ability=models.CharField(max_length=30)
-    weak=models.CharField(max_length=30)
-    description=models.CharField(max_length=300)
-
-class Avatar(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    imagen = models.ImageField(upload_to='avatar/', null=True, blank=True)
-
+class Pokemons(models.Model):
+    nombre=models.CharField(max_length=30)
+    numero=models.IntegerField()
+    tipo1=models.CharField(max_length=30)
+    tipo2=models.CharField(max_length=30)
+    imagen = models.ImageField(null=True, blank=True)
+    habilidad=models.CharField(max_length=30)
+    debilidad=models.CharField(max_length=30)
+  
+    
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    edad = models.CharField(max_length=30)
+    email= models.EmailField()
--- Pokedex/views.py
+++ Pokedex/views.py
@@ -4,8 +4,15 @@ from unicodedata import name
 from unittest import loader
 from xml.dom.minidom import Document
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from Pokedex.models import Pokemons, Usuarios
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.decorators import login_required
+import datetime
+from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -18,8 +25,8 @@ def about(request):
     return render(request, "about.html")
 
 
-def login(request):
-    return render(request, "login.html")
+##def login(request):
+##    return render(request, "login.html")
 
 
 def register(request):
@@ -43,3 +50,33 @@ def buscar_pokemon(request):
     else:
         respuesta = "No se enviaron datos"
     return HttpResponse(respuesta)
+
+
+def login_request(request):
+
+    if request.method == "POST":
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)                
+                return redirect("home")
+            else:                
+                return redirect("login")
+                
+        else:
+            return redirect("login")
+    
+    form = AuthenticationForm()
+
+    return render(request,"login.html",{"form":form})
+
+def logout_request(request):
+    logout(request)
+    return redirect("home")   
