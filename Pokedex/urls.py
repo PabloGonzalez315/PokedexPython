aad522c3efb77eaf2a0b7c35cabe934b9b3a2be5
pablo gonzalez <pablo.alejandro315@gmail.com>
2022-10-20 01:25:30 -0300

    crud pokemons Ok, mejorar visuales

--- Pokedex/urls.py
+++ Pokedex/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path("register/", register),
     path("pokemon/", pokemon),
     path("buscar_pokemon/", buscar_pokemon),
+    path('create_pokemons/', create_pokemons),
+    path('read_pokemons/', read_pokemons),
+    path('update_pokemons/<pokemon_id>', update_pokemons),
+    path('delete_pokemons/<pokemon_id>', delete_pokemons),
+    
 ]
 
